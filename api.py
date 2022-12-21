6a301aff813fb7b470d22cb792505406fbc96be2
giangcse <giangb1609816@student.ctu.edu.vn>
2022-12-21 12:23:29 +0700

    Tách các model ra thành các class

--- api.py
+++ api.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+#####################################################################################
+#   File API gồm các class chứa các Model Speech to text và model khử noise audio   #
+#   Cùng với class API chứa các API endpoint để tương tác với hệ thống              #
+#                                                                                   #
+#####################################################################################
 import json
 import os, zipfile
 import uvicorn
@@ -27,19 +32,103 @@ from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
+from speechbrain.pretrained import SepformerSeparation as separator
+import torchaudio
+
 class Delete_audio(BaseModel):
     username: str
     audio_name: str
 
+class BaseVietnamese_Model:
+    def __init__(self) -> None:
+        # Khoi tao model
+        self.cache_dir = './cache/'
+        self.processor = Wav2Vec2Processor.from_pretrained("models/wav2vec2-base-vietnamese-250h", local_files_only=True)
+        self.model = Wav2Vec2ForCTC.from_pretrained("models/wav2vec2-base-vietnamese-250h", local_files_only=True)
+        self.lm_file = "models/wav2vec2-base-vietnamese-250h/vi_lm_4grams.bin.zip"
+        print('[INFO]\t{}'.format('Model load completed'))
+        with zipfile.ZipFile(self.lm_file, 'r') as zip_ref:
+            zip_ref.extractall(self.cache_dir)
+        self.lm_file = self.cache_dir + 'vi_lm_4grams.bin'
+
+        self.ngram_lm_model = self.get_decoder_ngram_model(self.processor.tokenizer, self.lm_file)
+        print('[INFO]\t{}'.format('N-Grams models load completed'))
+
+    def get_decoder_ngram_model(self, tokenizer, ngram_lm_path):
+        vocab_dict = tokenizer.get_vocab()
+        sort_vocab = sorted((value, key) for (key, value) in vocab_dict.items())
+        vocab = [x[1] for x in sort_vocab][:-2]
+        vocab_list = vocab
+        # convert ctc blank character representation
+        vocab_list[tokenizer.pad_token_id] = ""
+        # replace special characters
+        vocab_list[tokenizer.unk_token_id] = ""
+        # vocab_list[tokenizer.bos_token_id] = ""
+        # vocab_list[tokenizer.eos_token_id] = ""
+        # convert space character representation
+        vocab_list[tokenizer.word_delimiter_token_id] = " "
+        # specify ctc blank char index, since conventially it is the last entry of the logit matrix
+        alphabet = Alphabet.build_alphabet(vocab_list, ctc_token_idx=tokenizer.pad_token_id)
+        lm_model = kenlm.Model(ngram_lm_path)
+        decoder = BeamSearchDecoderCTC(alphabet,
+                                    language_model=LanguageModel(lm_model))
+        return decoder
+
+    def wav2vec(self, y_mono, start, input_padding_len, chunk_len):
+        chunk = y_mono[start-input_padding_len:start+chunk_len+input_padding_len]
+        input_values = self.processor(
+            chunk, 
+            sampling_rate=16000, 
+            return_tensors="pt"
+        ).input_values
+        # ).input_values.to("cuda")
+        # model.to("cuda")
+        logits = self.model(input_values).logits[0]
+        # decode ctc output
+        pred_ids = torch.argmax(logits, dim=-1)
+        beam_search_output = self.ngram_lm_model.decode(logits.cpu().detach().numpy(), beam_width=500)
+        return beam_search_output
+
+    def split_audio_on_silence(self, audio_path):
+        #reading from audio mp3 file
+        sound = AudioSegment.from_mp3(audio_path)
+
+        # spliting audio files
+        audio_chunks = split_on_silence(sound, min_silence_len=500, silence_thresh=-40 )
+
+        # Create new folder with name of audio
+        head, tail = os.path.split(audio_path)
+        if not os.path.exists(os.path.join('static', tail.split('.')[0])):
+            os.mkdir(os.path.join('static', tail.split('.')[0]))
+        
+        list_of_dir = []
+        #loop is used to iterate over the output list
+        for i, chunk in enumerate(audio_chunks):
+            output_file = "chunk{0}.mp3".format(i)
+            chunk.export(os.path.join('static', tail.split('.')[0], output_file), format="mp3")
+            list_of_dir.append(os.path.join('static', tail.split('.')[0], output_file))
+        return list_of_dir
+
+class DenoiseAudio:
+    def __init__(self) -> None:
+        self.model = separator.from_hparams(source="speechbrain/sepformer-wham16k-enhancement", savedir='models/sepformer-wham16k-enhancement')
+
+    def denoise(self, audio_path):
+        if(os.path.exists(audio_path)):
+            est_sources = model.separate_file(path=audio_path) 
+            # torchaudio.save("fail_e.wav", est_sources[:, :, 0].detach().cpu(), 16000)
+            mono_audio = (est_sources[:, :, 0].detach().cpu())[0].numpy()
+            return mono_audio
+
+
 class API:
     def __init__(self) -> None:
         # Khởi tạo thông tin kết nối đến Database
-        print('*********** Database connection initial ***********')
         self.database = 'database.db'
         self.connection_db = sqlite3.connect(self.database, check_same_thread=False)
         self.cursor = self.connection_db.cursor()
+        print('[INFO]\t{}'.format('Connected to database'))
         # API Initial
-        print('*********** API Initial ***********')
         self.app = FastAPI()
         self.app.mount("/static", StaticFiles(directory="static"), name="static")
         self.templates = Jinja2Templates(directory="templates/")
@@ -50,22 +139,9 @@ class API:
             allow_methods=['*'],
             allow_headers=['*']
         )
-        # Model Initial
-        print('*********** Model Initial ***********')
-        self.cache_dir = './cache/'
-        self.processor = Wav2Vec2Processor.from_pretrained("models/wav2vec2-base-vietnamese-250h", local_files_only=True)
-        self.model = Wav2Vec2ForCTC.from_pretrained("models/wav2vec2-base-vietnamese-250h", local_files_only=True)
-        # self.lm_file = hf_bucket_url("models/wav2vec2-base-vietnamese-250h", filename='vi_lm_4grams.bin.zip')
-        # self.lm_file = cached_path(self.lm_file,cache_dir=self.cache_dir)
-        self.lm_file = "models/wav2vec2-base-vietnamese-250h/vi_lm_4grams.bin.zip"
-        print('*********** Model OK ***********')
-        print('*********** get_decoder_ngram_model ***********')
-        with zipfile.ZipFile(self.lm_file, 'r') as zip_ref:
-            zip_ref.extractall(self.cache_dir)
-        self.lm_file = self.cache_dir + 'vi_lm_4grams.bin'
+        print('[INFO]\t{}'.format('API initial'))
 
-        self.ngram_lm_model = self.get_decoder_ngram_model(self.processor.tokenizer, self.lm_file)
-        print('*********** get_decoder_ngram_model OK ***********')
+        self.BVM = BaseVietnamese_Model()
 
         @self.app.get("/")
         async def root(request: Request):
@@ -101,7 +177,6 @@ class API:
                 config = json.loads(f.read())
             while True:
                 data = await websocket.receive_text()
-                # audios = self.cursor.execute("SELECT AUDIO_NAME FROM audios WHERE USERNAME = ?", (str(data), )).fetchall()
                 y, sr = librosa.load(data, mono=False, sr=16000)
                 y_mono = librosa.to_mono(y)
                 chunk_duration = config['chunk_duration'] # sec
@@ -110,102 +185,11 @@ class API:
 
                 chunk_len = chunk_duration*sample_rate
                 input_padding_len = int(padding_duration*sample_rate)
-                output_padding_len = self.model._get_feat_extract_output_lengths(input_padding_len)
+                output_padding_len = self.BVM.model._get_feat_extract_output_lengths(input_padding_len)
 
-                beam_list = ''
-                all_preds = []
                 for start in range(input_padding_len, len(y_mono)-input_padding_len, chunk_len):
-                    chunk = y_mono[start-input_padding_len:start+chunk_len+input_padding_len]
-                    input_values = self.processor(
-                        chunk, 
-                        sampling_rate=sample_rate, 
-                        return_tensors="pt"
-                    ).input_values
-                    # ).input_values.to("cuda")
-                    # model.to("cuda")
-                    logits = self.model(input_values).logits[0]
-                    # decode ctc output
-                    pred_ids = torch.argmax(logits, dim=-1)
-                    # greedy_search_output = self.processor.decode(pred_ids)
-                    beam_search_output = self.ngram_lm_model.decode(logits.cpu().detach().numpy(), beam_width=500)
-                    await websocket.send_text(f"{beam_search_output}")
-
-    def get_decoder_ngram_model(self, tokenizer, ngram_lm_path):
-        vocab_dict = tokenizer.get_vocab()
-        sort_vocab = sorted((value, key) for (key, value) in vocab_dict.items())
-        vocab = [x[1] for x in sort_vocab][:-2]
-        vocab_list = vocab
-        # convert ctc blank character representation
-        vocab_list[tokenizer.pad_token_id] = ""
-        # replace special characters
-        vocab_list[tokenizer.unk_token_id] = ""
-        # vocab_list[tokenizer.bos_token_id] = ""
-        # vocab_list[tokenizer.eos_token_id] = ""
-        # convert space character representation
-        vocab_list[tokenizer.word_delimiter_token_id] = " "
-        # specify ctc blank char index, since conventially it is the last entry of the logit matrix
-        alphabet = Alphabet.build_alphabet(vocab_list, ctc_token_idx=tokenizer.pad_token_id)
-        lm_model = kenlm.Model(ngram_lm_path)
-        decoder = BeamSearchDecoderCTC(alphabet,
-                                    language_model=LanguageModel(lm_model))
-        return decoder
-
-    def map_to_array(self, batch):
-        speech, sampling_rate = sf.read(batch["file"])
-        batch["speech"] = speech
-        batch["sampling_rate"] = sampling_rate
-        return batch
-
-    def wav2vec_chunk(self, audio_path):
-        y, sr = librosa.load(audio_path, mono=False, sr=16000)
-        y_mono = librosa.to_mono(y)
-        chunk_duration = 10 # sec
-        padding_duration = 0 # sec
-        sample_rate = 16000
-
-        chunk_len = chunk_duration*sample_rate
-        input_padding_len = int(padding_duration*sample_rate)
-        output_padding_len = self.model._get_feat_extract_output_lengths(input_padding_len)
-
-        beam_list = ''
-        all_preds = []
-        for start in range(input_padding_len, len(y_mono)-input_padding_len, chunk_len):
-            chunk = y_mono[start-input_padding_len:start+chunk_len+input_padding_len]
-            input_values = self.processor(
-                chunk, 
-                sampling_rate=sample_rate, 
-                return_tensors="pt"
-            ).input_values
-            # ).input_values.to("cuda")
-            # model.to("cuda")
-            logits = self.model(input_values).logits[0]
-            # decode ctc output
-            pred_ids = torch.argmax(logits, dim=-1)
-            # greedy_search_output = self.processor.decode(pred_ids)
-            beam_search_output = self.ngram_lm_model.decode(logits.cpu().detach().numpy(), beam_width=500)
-            beam_list+= (beam_search_output) + ' '
-        return beam_list
-
-    def split_audio_on_silence(self, audio_path):
-        #reading from audio mp3 file
-        sound = AudioSegment.from_mp3(audio_path)
-
-        # spliting audio files
-        audio_chunks = split_on_silence(sound, min_silence_len=500, silence_thresh=-40 )
-
-        # Create new folder with name of audio
-        head, tail = os.path.split(audio_path)
-        if not os.path.exists(os.path.join('static', tail.split('.')[0])):
-            os.mkdir(os.path.join('static', tail.split('.')[0]))
-        
-        list_of_dir = []
-        #loop is used to iterate over the output list
-        for i, chunk in enumerate(audio_chunks):
-            output_file = "chunk{0}.mp3".format(i)
-            chunk.export(os.path.join('static', tail.split('.')[0], output_file), format="mp3")
-            list_of_dir.append(os.path.join('static', tail.split('.')[0], output_file))
-        return list_of_dir
-            
+                    await websocket.send_text(f"{self.BVM.wav2vec(y_mono, start, input_padding_len, chunk_len)}")
+  
 
 api = API()
 
