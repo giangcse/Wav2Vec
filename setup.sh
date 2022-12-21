49007391af8ac664709ad8adcfad5f20f5114e22
giangcse <giangb1609816@student.ctu.edu.vn>
2022-12-21 07:41:44 +0700

    Thêm ffmpeg

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ if [ "$UID" -eq 0 -o "$EUID" -eq 0 ]; then
     apt install python3.7-dev -y
     apt install python3.7-venv -y
     apt install python3-pip -y
+    apt install ffmpeg -y
 else
     echo "Please run with sudo"
 fi
