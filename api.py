69ccc5814b207c40e319fdc22001cc2db4b544b2
giangcse <giangb1609816@student.ctu.edu.vn>
2022-12-27 09:41:00 +0700

    Đã thêm so sánh 2 model

--- api.py
+++ api.py
@@ -7,6 +7,8 @@ import sqlite3
 import datetime
 import websockets
 import asyncio
+import difflib
+import re
 
 from fastapi import FastAPI, Form, File, UploadFile, WebSocket
 from fastapi.requests import Request
@@ -99,28 +101,97 @@ class API:
             while True:
                 data = await websocket.receive_text()
                 data = json.loads(data)
-                return_list_1 = []
-                return_list_2 = []
-                return_data = []
+                return_string_1 = ''
+                return_string_2 = ''
+                return_data = None
                 if(data['model']=='vlsp'):
                     return_data = self.VLSP.speech_to_text(data)
                     for i in return_data:
-                        return_list_1.append([str(i)])
+                        await websocket.send_text(str(i))
                 elif(data['model']=='250h'):
                     return_data = self.BVM.speech_to_text(data)
                     for i in return_data:
-                        return_list_2.append([str(i)])
+                        await websocket.send_text(str(i))
                 else:
                     for i in self.VLSP.speech_to_text(data):
-                        return_list_1.append([str(i)])
+                        return_string_1 += (str(i))
                     for j in self.BVM.speech_to_text(data):
-                        return_list_2.append([str(j)])
+                        return_string_2 += (str(j))
+                    # self.show_comparison(return_string_1, return_string_2, sidebyside=False)
+                    await websocket.send_text(self.show_comparison(return_string_1, return_string_2, sidebyside=False))
 
-                return_data.append(return_list_1)
-                return_data.append(return_list_2)
-                await websocket.send_text(str(return_data))
+    def tokenize(self, s):
+        return re.split('\s+', s)
+    
+    def untokenize(self, ts):
+        return ' '.join(ts)
+            
+    def equalize(self, s1, s2):
+        l1 = self.tokenize(s1)
+        l2 = self.tokenize(s2)
+        res1 = []
+        res2 = []
+        prev = difflib.Match(0,0,0)
+        for match in difflib.SequenceMatcher(a=l1, b=l2).get_matching_blocks():
+            if (prev.a + prev.size != match.a):
+                for i in range(prev.a + prev.size, match.a):
+                    res2 += ['_' * len(l1[i])]
+                res1 += l1[prev.a + prev.size:match.a]
+            if (prev.b + prev.size != match.b):
+                for i in range(prev.b + prev.size, match.b):
+                    res1 += ['_' * len(l2[i])]
+                res2 += l2[prev.b + prev.size:match.b]
+            res1 += l1[match.a:match.a+match.size]
+            res2 += l2[match.b:match.b+match.size]
+            prev = match
+        return self.untokenize(res1), self.untokenize(res2)
 
+    def insert_newlines(self, string, every=64, window=10):
+        result = []
+        from_string = string
+        while len(from_string) > 0:
+            cut_off = every
+            if len(from_string) > every:
+                while (from_string[cut_off-1] != ' ') and (cut_off > (every-window)):
+                    cut_off -= 1
+            else:
+                cut_off = len(from_string)
+            part = from_string[:cut_off]
+            result += [part]
+            from_string = from_string[cut_off:]
+        return result
 
+    def show_comparison(self, s1, s2, width=40, margin=10, sidebyside=True, compact=False):
+        s1, s2 = self.equalize(s1,s2)
+
+        if sidebyside:
+            s1 = self.insert_newlines(s1, width, margin)
+            s2 = self.insert_newlines(s2, width, margin)
+            if compact:
+                for i in range(0, len(s1)):
+                    lft = re.sub(' +', ' ', s1[i].replace('_', '')).ljust(width)
+                    rgt = re.sub(' +', ' ', s2[i].replace('_', '')).ljust(width) 
+                    print(lft + ' | ' + rgt + ' | ')        
+            else:
+                for i in range(0, len(s1)):
+                    lft = s1[i].ljust(width)
+                    rgt = s2[i].ljust(width)
+                    print(lft + ' | ' + rgt + ' | ')
+        else:
+            sentence_1 = s1.split(' ')
+            sentence_2 = s2.split(' ')
+            return_string = ''
+            for i in range(len(sentence_1)):
+                if str(sentence_1[i]).lower() == str(sentence_2[i]).lower():
+                    return_string += "{} ".format(sentence_1[i])
+                else:
+                    if '_' in sentence_1[i]:
+                        return_string += '{} '.format(sentence_2[i])
+                    elif '_' in sentence_2[i]:
+                        return_string += '{} '.format(sentence_1[i])
+                    else:
+                        return_string += '[{} | {}] '.format(sentence_1[i], sentence_2[i])
+            return return_string
 
 api = API()
 
