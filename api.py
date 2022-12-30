0970578dda8ae357b0c0bf2f3140e679d4e56e76
giangcse <giangb1609816@student.ctu.edu.vn>
2022-12-30 14:24:31 +0700

    change port from 9089 to 9090

--- api.py
+++ api.py
@@ -210,4 +210,4 @@ class API:
 api = API()
 
 if __name__=='__main__':
-    uvicorn.run('api:api.app', host='0.0.0.0', port=9089, reload=True)
+    uvicorn.run('api:api.app', host='0.0.0.0', port=9090, reload=True)
